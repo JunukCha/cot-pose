73ce8f331ab0af2a4cc940864c7fbf3a258c1b6a
root <jucha@unist.ac.kr>
2025-08-19 18:06:29 +0900

    posescript install

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,3 +8,7 @@ conda activate cot-pose
 pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu121
 
 pip install -r requirements.txt
+
+cd posescript
+python setup.py develop
+cd ..
